b1a90aa0d81eb1cd63ce6fe067803fb45115345c
Jonas Fovea <30690893+JonasFovea@users.noreply.github.com>
2020-07-25 01:43:51 +0200

    Getter fuer Namen und Listen von Klassen und Funktionen in der Doc-Klasse

--- docBuilder.py
+++ docBuilder.py
@@ -1,5 +1,7 @@
 class Doc:
-    name = None
+    name = ""
+    classes = []
+    functions = []
 
     def __init__(self, name=None):
         self.name = name
@@ -7,17 +9,31 @@ class Doc:
     def getName(self):
         return self.name
 
-    def addClass(self, name):
-        pass
+    def addClass(self, argClass):
+        self.classes.append(argClass)
 
+    def getClasses(self):
+        return self.classes
+
+    def addFunction(self, func):
+        self.functions.append(func)
+
+    def getFunctions(self):
+        return self.functions
 
 class Class:
+    name = ""
     def __init__(self, name=None):
-        pass
+        self.name = name
+
+    def getName(self):
+        return self.name
 
 
 class Function:
-    pass
+    name = ""
+    def __init__(self, name=None):
+        self.name = name
 
 
 class Field:
--- test_docbuilder.py
+++ test_docbuilder.py
@@ -12,7 +12,8 @@ class MyTestCase(unittest.TestCase):
         tClass2 = Class("TestClass2")
 
     def test_createFunction(self):
-        tFunc = Function()
+        tFunc1 = Function()
+        tFunc2 = Function("Function2")
 
     def test_createField(self):
         tField1 = Field()
@@ -20,13 +21,43 @@ class MyTestCase(unittest.TestCase):
 
     def test_addClass(self):
         tDoc = Doc()
-        tDoc.addClass("Test1")
+        tDoc.addClass(Class("Test1"))
+        tDoc.addClass(Class("Test2"))
 
     def test_DocGetName(self):
         tDoc1 = Doc("TestName")
         self.assertEqual(tDoc1.getName(), "TestName")
         tDoc2 = Doc("TestNameABC")
         self.assertEqual(tDoc2.getName(), "TestNameABC")
+
+    def test_ClassGetName(self):
+        tClass1 = Class("TestName")
+        self.assertEqual(tClass1.getName(), "TestName")
+        tClass2 = Class("TestNameABC")
+        self.assertEqual(tClass2.getName(), "TestNameABC")
+
+    def test_DocGetClasses(self):
+        tDoc = Doc("Documentation")
+        tC1 = Class("Class1")
+        tC2 = Class("Class2")
+        tDoc.addClass(tC1)
+        tDoc.addClass(tC2)
+        self.assertListEqual(tDoc.getClasses(), [tC1, tC2])
+
+    def test_DocAddFunction(self):
+        tDoc = Doc("Documentation")
+        tFunction1 = Function()
+        tFunction2 = Function("Function2")
+        tDoc.addFunction(tFunction1)
+        tDoc.addFunction(tFunction2)
+
+    def test_DocGetFunctions(self):
+        tDoc = Doc("Documentation")
+        tFunction1 = Function()
+        tFunction2 = Function("Function2")
+        tDoc.addFunction(tFunction1)
+        tDoc.addFunction(tFunction2)
+        self.assertListEqual(tDoc.getFunctions(), [tFunction1, tFunction2])
 
 
 if __name__ == '__main__':
