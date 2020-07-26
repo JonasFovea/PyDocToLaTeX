9e7c284e8e3ce91e4e4e6da6e360450cbbf5c9f5
Jonas Fovea <30690893+JonasFovea@users.noreply.github.com>
2020-07-26 12:07:59 +0200

    Weitere Grundfunktionen und Grundstruktur hinzugefuegt

--- docBuilder.py
+++ docBuilder.py
@@ -2,6 +2,7 @@ class Doc:
     name = ""
     classes = []
     functions = []
+    fields = []
 
     def __init__(self, name=None):
         self.name = name
@@ -21,21 +22,100 @@ class Doc:
     def getFunctions(self):
         return self.functions
 
+    def addField(self, field):
+        self.fields.append(field)
+
+    def getFields(self):
+        return self.fields
+
+
 class Class:
     name = ""
+    description = ""
+    functions = []
+    fields = []
+
     def __init__(self, name=None):
         self.name = name
 
     def getName(self):
         return self.name
 
+    def addFunction(self, func):
+        self.functions.append(func)
+
+    def getFunctions(self):
+        return self.functions
+
+    def addField(self, field):
+        self.fields.append(field)
+
+    def getFields(self):
+        return self.fields
+
+    def setDescription(self, desc: str):
+        self.description = desc
+
+    def getDescription(self):
+        return self.description
+
 
 class Function:
-    name = ""
-    def __init__(self, name=None):
+    description = None
+    parameters = []
+
+    def __init__(self, name=""):
         self.name = name
+
+    def getName(self):
+        return self.name
+
+    def setDescription(self, descr: str):
+        self.description = descr
+
+    def getDescription(self):
+        return self.description
+
+    def addParameter(self, param):
+        self.parameters.append(param)
+
+    def getParameters(self):
+        return self.parameters
 
 
 class Field:
-    def __init__(self, name=None):
-        pass
+    type = "ANY"
+    def __init__(self, name=""):
+        self.name = name
+
+    def getName(self):
+        return self.name
+
+    def setType(self, type: str):
+        self.type = type
+
+    def getType(self):
+        return self.type
+
+
+class Parameter:
+    description = None
+    type = "ANY"
+
+    def __init__(self, name=""):
+        self.name = name
+
+    def getName(self):
+        return self.name
+
+    def setDescription(self, descr: str):
+        self.description = descr
+
+    def getDescription(self):
+        return self.description
+
+    def setType(self, type: str):
+        self.type = type
+
+    def getType(self):
+        return self.type
--- test_docbuilder.py
+++ test_docbuilder.py
@@ -19,10 +19,9 @@ class MyTestCase(unittest.TestCase):
         tField1 = Field()
         tField2 = Field("TestField2")
 
-    def test_addClass(self):
-        tDoc = Doc()
-        tDoc.addClass(Class("Test1"))
-        tDoc.addClass(Class("Test2"))
+    def test_CreateParameter(self):
+        tParam1 = Parameter()
+        tParam2 = Parameter("Parameter2")
 
     def test_DocGetName(self):
         tDoc1 = Doc("TestName")
@@ -35,6 +34,29 @@ class MyTestCase(unittest.TestCase):
         self.assertEqual(tClass1.getName(), "TestName")
         tClass2 = Class("TestNameABC")
         self.assertEqual(tClass2.getName(), "TestNameABC")
+
+    def test_FunctionGetName(self):
+        tFunc1 = Function()
+        self.assertEqual(tFunc1.getName(), "")
+        tFunc2 = Function("Function2")
+        self.assertEqual(tFunc2.getName(), "Function2")
+
+    def test_FieldGetName(self):
+        tField1 = Field()
+        self.assertEqual(tField1.getName(), "")
+        tField2 = Field("Field2")
+        self.assertEqual(tField2.getName(), "Field2")
+
+    def test_ParameterGetName(self):
+        tParam1 = Parameter()
+        self.assertEqual(tParam1.getName(), "")
+        tParam2 = Parameter("Parameter2")
+        self.assertEqual(tParam2.getName(), "Parameter2")
+
+    def test_DocAddClass(self):
+        tDoc = Doc()
+        tDoc.addClass(Class("Test1"))
+        tDoc.addClass(Class("Test2"))
 
     def test_DocGetClasses(self):
         tDoc = Doc("Documentation")
@@ -59,6 +81,110 @@ class MyTestCase(unittest.TestCase):
         tDoc.addFunction(tFunction2)
         self.assertListEqual(tDoc.getFunctions(), [tFunction1, tFunction2])
 
+    def test_DocAddField(self):
+        tDoc = Doc("Dokumentation")
+        tF1 = Field()
+        tF2 = Field("Field2")
+        tDoc.addField(tF1)
+        tDoc.addField(tF2)
+
+    def test_DocGetFields(self):
+        tDoc = Doc("Dokumentation")
+        tF1 = Field()
+        tF2 = Field("Field2")
+        tDoc.addField(tF1)
+        tDoc.addField(tF2)
+        self.assertListEqual(tDoc.getFields(), [tF1, tF2])
+
+    def test_ClassAddFunction(self):
+        tClass = Class("Test Class")
+        tFunc1 = Function("Function1")
+        tFunc2 = Function("Function2")
+        tClass.addFunction(tFunc1)
+        tClass.addFunction(tFunc2)
+
+    def test_ClassGetFunctions(self):
+        tClass = Class("Test Class")
+        tFunc1 = Function("Function1")
+        tFunc2 = Function("Function2")
+        tClass.addFunction(tFunc1)
+        tClass.addFunction(tFunc2)
+        self.assertListEqual(tClass.getFunctions(), [tFunc1, tFunc2])
+
+    def test_ClassAddField(self):
+        tClass = Class("Test Class")
+        tField1 = Field("Field1")
+        tField2 = Field("Field2")
+        tClass.addField(tField1)
+        tClass.addField(tField2)
+
+    def test_ClassGetFields(self):
+        tClass = Class("Test Class")
+        tField1 = Field("Field1")
+        tField2 = Field("Field2")
+        tClass.addField(tField1)
+        tClass.addField(tField2)
+        self.assertListEqual(tClass.getFields(), [tField1, tField2])
+
+    def test_ParameterSetDescription(self):
+        tParam = Parameter("x")
+        tParam.setDescription("x-Coordinate")
+
+    def test_ParameterGetDescription(self):
+        tParam = Parameter("x")
+        tParam.setDescription("x-Coordinate")
+        self.assertEqual(tParam.getDescription(), "x-Coordinate")
+
+    def test_ParameterSetType(self):
+        tParam = Parameter("Test Parameter")
+        tParam.setType("int")
+
+    def test_ParameterGetType(self):
+        tParam = Parameter("Test Parameter")
+        tParam.setType("int")
+        self.assertEqual(tParam.getType(), "int")
+
+    def test_FunctionSetDescription(self):
+        tFunc = Function("Test Function")
+        tFunc.setDescription("Function returns the Sum of the Parameters a and b.")
+
+    def test_FunctionGetDescription(self):
+        tFunc = Function("Test Function")
+        tFunc.setDescription("Function returns the Sum of the Parameters a and b.")
+        self.assertEqual(tFunc.getDescription(), "Function returns the Sum of the Parameters a and b.")
+
+    def test_FunctionAddParameter(self):
+        tFunc = Function()
+        tParam1 = Parameter("Param 1")
+        tParam2 = Parameter("Parameter 2")
+        tFunc.addParameter(tParam1)
+        tFunc.addParameter(tParam2)
+
+    def test_FunctionGetParameters(self):
+        tFunc = Function()
+        tParam1 = Parameter("Param 1")
+        tParam2 = Parameter("Parameter 2")
+        tFunc.addParameter(tParam1)
+        tFunc.addParameter(tParam2)
+        self.assertListEqual(tFunc.getParameters(), [tParam1, tParam2])
+
+    def test_ClassSetDescription(self):
+        tClass = Class()
+        tClass.setDescription("Lorem ipsum dolor sit amet")
+
+    def test_ClassGetDescription(self):
+        tClass = Class()
+        tClass.setDescription("Lorem ipsum dolor sit amet")
+        self.assertEqual(tClass.getDescription(), "Lorem ipsum dolor sit amet")
+
+    def test_FieldSetType(self):
+        tField = Field("Test Field")
+        tField.setType("int")
+
+    def test_FieldGetType(self):
+        tField = Field("Test Field")
+        tField.setType("int")
+        self.assertEqual(tField.getType(), "int")
 
 if __name__ == '__main__':
     unittest.main()
