8604916a061c878a5e36e08b62c89e7b70e1323f
Jonas Fovea <30690893+JonasFovea@users.noreply.github.com>
2020-08-22 14:37:21 +0200

    CLI Support

--- pyToTeX.py
+++ pyToTeX.py
@@ -1,9 +1,12 @@
-import re
 import os
-from extractor import *
-from docBuilder import *
+import sys
 
-FILENAME_RE = re.compile(r'(\w+)\.py')
+from docBuilder import *
+from extractor import *
+
+FILENAME_RE = re.compile(r'(\w+)\.py$')
+PATH_RE = re.compile(r'(.*)?(\w+)\.py$')
+OVERRIDE_OPTION_RE = re.compile(r'-o$')
 
 
 def convert(fileName: str, override=False):
@@ -54,3 +57,34 @@ def convert(fileName: str, override=False):
             doc.addClass(texClass)
 
         doc.exportFile(texName)
+
+
+if __name__ == '__main__':
+    argv = sys.argv
+    if len(argv) == 1:
+        print(
+            "\tERROR: Please provide a path to a .py file, which you want to convert.\nAdditional option for overwriting existing .tex files: -o")
+    elif len(argv) == 2:
+        if re.search(PATH_RE, argv[1].strip()):
+            try:
+                convert(argv[1].strip())
+                nameMatch = re.search(PATH_RE, argv[1].strip())
+                print("\tSUCCESS: Converted " + nameMatch.group(1) + nameMatch.group(2) + ".py into " + nameMatch.group(
+                    1) + nameMatch.group(2) + ".tex")
+            except Exception as e:
+                print("\tERROR: " + str(e))
+        else:
+            print("\tERROR: Please provide a valid filepath.")
+    elif len(argv) == 3:
+        if re.search(PATH_RE, argv[1].strip()) and re.search(OVERRIDE_OPTION_RE, argv[2].strip()):
+            try:
+                convert(argv[1].strip(), True)
+                nameMatch = re.search(PATH_RE, argv[1].strip())
+                print("\tSUCCESS: Converted " + nameMatch.group(1) + nameMatch.group(2) + ".py into " + nameMatch.group(
+                    1) + nameMatch.group(2) + ".tex")
+            except Exception as e:
+                print("\tERROR: " + str(e))
+        else:
+            print("\tERROR: Please provide valid arguments.\nAdditional option for overwriting existing .tex files: -o")
+    else:
+        print("\tERROR: Please provide valid arguments.\nAdditional option for overwriting existing .tex files: -o")
